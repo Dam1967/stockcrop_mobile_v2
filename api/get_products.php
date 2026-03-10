dbb9e36afdc89c00fdae3b4f9cd859821d4ca171
David Martin <104455264+Dam1967@users.noreply.github.com>
2026-03-10 08:45:01 -0500

    Update JSON response message in get_products.php

--- api/get_products.php
+++ api/get_products.php
@@ -3,11 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-header('Content-Type: application/json; charset=UTF-8');
-
-require_once 'config.php';
-
 echo json_encode([
     "success" => true,
-    "message" => "get_products.php is running"
+    "message" => "plain php works"
 ]);
