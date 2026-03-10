959ddc3543f87aa0d1e82eeb9d5c53804afd0fd1
David Martin <104455264+Dam1967@users.noreply.github.com>
2026-03-10 08:29:23 -0500

    Add error reporting to get_product_details.php
    Enable error reporting for debugging purposes.

--- api/get_product_details.php
+++ api/get_product_details.php
@@ -1,4 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header('Content-Type: application/json; charset=UTF-8');
+
+require_once 'config.php';
+
 header("Content-Type: application/json; charset=UTF-8");
 
 require_once 'config.php';
